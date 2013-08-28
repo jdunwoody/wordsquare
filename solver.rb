4d9aa4a4028323e15a2429082c63cdbe2177a0bf
James Dunwoody <jamesd@hooroo.com>
2013-08-28 20:52:06 +1000

    Initial commit

---
+++ solver.rb
@@ -0,0 +1,25 @@
+correct_chars_regexp = Regexp.new(/^[satipndem]+$/i)
+duplicate_chars_regexp = Regexp.new(/(\w).*\1/i)
+
+count = 0
+dictionary = '/usr/share/dict/words'
+#dictionary = 'en.txt'
+longest = []
+length = 0
+
+File.foreach(dictionary) do |line|
+  if line.match(correct_chars_regexp) && !line.match(duplicate_chars_regexp)
+    line = line.chomp
+
+    if line.length > length
+      longest = [line]
+      length = line.length
+    elsif line.length == 9
+      longest << line
+    end
+    count = count + 1
+  end
+end
+puts "#{count} words matched"
+puts "longest word length: #{length}"
+puts "longest words are #{longest}"
